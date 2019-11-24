7740f3947b3c8df44433e14864f1c08b69efdba9
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-11-25 02:42:07 +0700

    Edit Interface

--- interface/GUI.py
+++ interface/GUI.py
@@ -17,24 +17,34 @@ SHAPES = [
     'Segi Enam Sama Sisi',
 ]
 
+
 def showSelectedImage(path):
-    window.FindElement('_BOX1_').Update(image_filename=path, image_subsample=10, image_size=(500, 450), button_color=sg.TRANSPARENT_BUTTON)
+    window.FindElement('_BOX1_').Update(image_filename=path, image_subsample=10, image_size=(500, 450),
+                                        button_color=sg.TRANSPARENT_BUTTON)
     window.refresh()
 
+
 def showSelectedShape(val):
-    window.FindElement('_BOX2_').Update(image_filename="img/"+str(val)+".png", image_subsample=10, image_size=(500, 450), button_color=sg.TRANSPARENT_BUTTON)
+    window.FindElement('_BOX2_').Update(image_filename="img/" + str(val) + ".png", image_subsample=10,
+                                        image_size=(500, 450), button_color=sg.TRANSPARENT_BUTTON)
     window.refresh()
+
 
 tree_layout = [[sg.Listbox(values=SHAPES, size=(30, 13), key="_CHOOSESHAPE_", enable_events=True)]]
 
 window = sg.Window('columns')
 
-# btn1 = sg.FileBrowse( button_text="Open Image", size=(14,1), key="btn1", button_color=('white','#475841'))
-btn1 = sg.FileBrowse(file_types=(("Image Files", "*.png"),), button_text="Open Image", size=(30,1), key="_CHOOSE_",
+label1 = sg.T("Source Image", size=(30, 1),pad=((0,300),(0,0)), background_color="#f3f3f3")
+label2 = sg.T("Detection Image", size=(30, 1), background_color="#f3f3f3")
+label3 = sg.T("Detection Results", size=(30, 1),pad=((0,270),(0,0)), background_color="#f3f3f3")
+label4 = sg.T("Matched Facts", size=(30, 1),pad=((0,270),(0,0)), background_color="#f3f3f3")
+label5 = sg.T("Hit Rules", size=(30, 1),pad=((0,270),(0,0)), background_color="#f3f3f3")
+
+btn1 = sg.FileBrowse(file_types=(("Image Files", "*.png"),), button_text="Open Image", size=(30, 1), key="_CHOOSE_",
                      enable_events=True, button_color=('white', '#475841'))
-btn2 = sg.Button(button_text="Open Rule Editor", size=(30,1), key="btn2", button_color=('white', '#475841'))
-btn3 = sg.Button(button_text="Show Rules", size=(30,1), key="btn3", button_color=('white', '#475841'))
-btn4 = sg.Button(button_text="Show Facts", size=(30,1), key="btn4", button_color=('white', '#475841'))
+btn2 = sg.Button(button_text="Open Rule Editor", size=(30, 1), key="btn2", button_color=('white', '#475841'))
+btn3 = sg.Button(button_text="Show Rules", size=(30, 1), key="btn3", button_color=('white', '#475841'))
+btn4 = sg.Button(button_text="Show Facts", size=(30, 1), key="btn4", button_color=('white', '#475841'))
 button_layout = [
     [btn1],
     [btn2],
@@ -42,21 +52,25 @@ button_layout = [
     [btn4]
 ]
 
-button_wrapper = sg.Frame(title="Menu", layout=button_layout, size=(366,225), background_color="white")
-tree_wrapper = sg.Frame(title="What Shape Do You Want?", layout=tree_layout, size=(366,225), background_color="white")
+button_wrapper = sg.Frame(title="Menu", layout=button_layout, size=(366, 225), background_color="white")
+tree_wrapper = sg.Frame(title="What Shape Do You Want?", layout=tree_layout, size=(366, 225), background_color="white")
 option_layout = [
     [button_wrapper],
     [tree_wrapper]
 ]
 
-box1 = sg.Button(image_filename="white.png", image_size=(500, 450), auto_size_button=False,key="_BOX1_", size=(500, 450))
-box2 = sg.Button(image_filename="white.png", image_size=(500, 450), auto_size_button=False,key="_BOX2_", size=(500, 450))
-menu_wrapper = sg.Frame(title="Menu", layout=option_layout, size=(366,450), background_color="#f3f3f3", border_width=0)
+box1 = sg.Button(image_filename="white.png", image_size=(500, 450), auto_size_button=False, key="_BOX1_",
+                 size=(500, 450), border_width=3)
+box2 = sg.Button(image_filename="white.png", image_size=(500, 450), auto_size_button=False, key="_BOX2_",
+                 size=(500, 450), border_width=3)
+menu_wrapper = sg.Frame(title="", layout=option_layout, size=(366, 450), background_color="#f3f3f3", border_width=0)
 box3 = sg.Image(filename="white.png", key="box3", size=(455, 318))
 box4 = sg.Image(filename="white.png", key="box4", size=(455, 318))
 box5 = sg.Image(filename="white.png", key="box5", size=(455, 318))
 layout = [
+    [label1, label2],
     [box1, box2, menu_wrapper],
+    [label3, label4, label5],
     [box3, box4, box5]
 ]
 
